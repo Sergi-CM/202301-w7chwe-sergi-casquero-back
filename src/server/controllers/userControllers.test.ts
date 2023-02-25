2f8f93076990d6c70d4560e01168d47e098eae8e
Sergi Casquero <sergicasquero@gmail.com>
2023-02-25 17:08:29 +0100

    Add createUser controller test

--- src/server/controllers/userControllers.test.ts
+++ src/server/controllers/userControllers.test.ts
@@ -1,6 +1,7 @@
-import { type Request, type Response } from "express";
+import { request, type Request, type Response } from "express";
 import User from "../../database/models/User";
-import { getUsers } from "./userControllers";
+import { type UserRegister } from "../types";
+import { createUser, getUsers } from "./userControllers";
 
 describe("Given the getUsers function", () => {
   describe("When it receives a res object", () => {
@@ -15,6 +16,33 @@ describe("Given the getUsers function", () => {
       }));
 
       await getUsers(req, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+  });
+});
+
+describe("Given the createUser function", () => {
+  describe("when it receives a req object with a robot data and a res object", () => {
+    test("Then it should call its status method with 201", async () => {
+      const newUser: UserRegister = {
+        name: "Marius",
+        username: "marius69",
+        email: "marius@isdi.com",
+        password: "l@sDevt00ls!",
+        avatar: "mariuswithcapucha.jpg",
+      };
+      const req = { body: newUser } as Request<
+        Record<string, unknown>,
+        Record<string, unknown>,
+        UserRegister
+      >;
+      const res = { status: jest.fn() } as Partial<Response>;
+      const next = () => ({});
+      const statusCode = 201;
+
+      User.create = jest.fn().mockReturnValue(newUser);
+      await createUser(req, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode);
     });
